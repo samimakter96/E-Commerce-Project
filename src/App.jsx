0662a83d58002b3bd29d9bdb8294e436d0ebecb7
samimakter96 <samimakter9641@gmail.com>
2024-05-15 09:36:04 +0530

    added lazy loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { Suspense, lazy, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
-import Contents from './components/Contents';
-import Cart from './components/Cart/Cart';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import ContactUs from './pages/contact/ContactUs';
-import ProductDetail from './pages/ProductPage/ProductDetails';
-import LogIn from './pages/LogIn/LogIn';
 import { CartProvider } from './components/context/CartContext';
 import { AuthProvider } from './pages/LogIn/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Contents from './components/Contents';
+// import Cart from './components/Cart/Cart';
+// import Home from './pages/Home/Home';
+// import About from './pages/About/About';
+// import ContactUs from './pages/contact/ContactUs';
+// import ProductDetail from './pages/ProductPage/ProductDetails';
+// import LogIn from './pages/LogIn/LogIn';
+
+// User lazy for components to be loaded lazily This approach helps improve the performance of your application by only loading the necessary components when they are needed, reducing the initial load time.
+
+const Contents = lazy(() => import('./components/Contents'));
+const Cart = lazy(() => import('./components/Cart/Cart'));
+const Home = lazy(() => import('./pages/Home/Home'));
+const About = lazy(() => import('./pages/About/About'));
+const ContactUs = lazy(() => import('./pages/contact/ContactUs'));
+const ProductDetail = lazy(() => import('./pages/ProductPage/ProductDetails'));
+const LogIn = lazy(() => import('./pages/LogIn/LogIn'));
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -30,17 +41,23 @@ function App() {
         <AuthProvider>
           <CartProvider>
             <Header toggleCart={toggleCart} />
-            <Routes>
-              <Route path="/" element={<Contents />} />
-              <Route path="/store" element={<Contents />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<ContactUs />} />
-              <Route path="/login" element={<LogIn />} />
-              <Route path="/product/:productId" element={<ProductDetail />} />
-            </Routes>
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <Routes>
+                <Route path="/" element={<Contents />} />
+                <Route path="/store" element={<Contents />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<ContactUs />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/product/:productId" element={<ProductDetail />} />
+              </Routes>
+            </Suspense>
             <Footer />
-            {showCart && <Cart closeCart={closeCart} />}
+            {showCart && (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Cart closeCart={closeCart} />
+              </Suspense>
+            )}
           </CartProvider>
         </AuthProvider>
       </Router>
